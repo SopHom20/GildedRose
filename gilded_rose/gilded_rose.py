b58f7a44dcf7854ff6fcbaa8e952e4bbbc414a4c
Sophia Ho <Sophia.HoMeiYin@softwire.com>
2022-07-13 12:19:40 +0100

    Final Version; refactored sell_in date

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -6,8 +6,9 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             if not self.is_quality_increasing(item.name):
-                if item.name != "Sulfuras, Hand of Ragnaros" and item.quality >0:
-                    item.quality -= 1
+                if item.name != "Sulfuras, Hand of Ragnaros":
+                    new_item = self.decrease_amount_quality(item)
+                    item = new_item
 
             else:
                 new_item = self.increase_amount_quality(item)
@@ -15,17 +16,7 @@ class GildedRose(object):
 
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+
 
     def is_quality_increasing(self, item_name):
         if item_name == "Aged Brie" or item_name =="Backstage passes to a TAFKAL80ETC concert":
@@ -41,7 +32,7 @@ class GildedRose(object):
         if self.is_item_backstage(item):
             amount_increase = self.amount_increase_backstage(item)
         else:
-            amount_increase = 1
+            amount_increase = self.double_speed_quality(item)
         item.quality += amount_increase
         if item.quality > 50:
             item.quality = 50
@@ -52,7 +43,21 @@ class GildedRose(object):
             return 1
         elif item.sell_in > 5:
             return 2
-        elif item.sell_in >= 0:
+        elif item.sell_in > 0:
             return 3
         else:
             return -item.quality
+
+    def decrease_amount_quality(self,item):
+        item.quality -= self.double_speed_quality(item)
+        if item.quality < 0:
+            item.quality = 0
+        return item
+
+    def double_speed_quality(self, item):
+        amount_change = 1
+
+        if item.sell_in <= 0:
+            amount_change = amount_change * 2
+
+        return amount_change
